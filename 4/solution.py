14c173f67875c90ccc99c6f19e94ab85cfe11980
Sam Wheating <samwheating@gmail.com>
2023-12-03 21:46:12 -0800

    cleanup + run faster

--- 4/solution.py
+++ 4/solution.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Iterable
+from typing import Dict
 from collections import defaultdict
 import sys
 
@@ -8,47 +8,36 @@ else:
     file = "input.txt"
 
 with open(file) as inputfile:
-    lines = [l.strip('\n') for l in inputfile.readlines()]
+    lines = [l.strip("\n") for l in inputfile.readlines()]
 
 total = 0
 matches_per_card: Dict[int, int] = {}
 for i in range(len(lines)):
+    # hacky parsing, but it works.
+    winners = [int(s) for s in lines[i].split(" | ")[0].split(":")[1].split()]
+    numbers = [int(s) for s in lines[i].split(" | ")[1].split()]
 
-    winners = []
-    numbers = []
-    for section in lines[i].split(" | ")[0].split(":")[1].split():
-        winners.append(int(section))
-    for section in lines[i].split(" | ")[1].split():
-        numbers.append(int(section))
-    
-    matches = 0
-    for num in numbers:
-        if num in winners:
-            matches += 1
-
-    matches_per_card[i+1] = matches # 0-idx to 1-idx, store these for part 2
-    if matches > 0:
-        total += 2**(matches-1)
+    matches = len([n for n in numbers if n in winners])
+    matches_per_card[i + 1] = matches  # 0-idx to 1-idx, store these for part 2
+    total += int(2 ** (matches - 1))
 
 print(f"Part 1: {total}")
-        
+
 # Part 2: Just simulate all of the cards, running one hand at a time
 # I'm pretty sure there's a faster more math-y solution here but I was in a hurry
-# (takes ~1.5s on MBP w/ M1 Max LOL)
 
 # current hand of cards (card: number in hand)
-cards = {c: 1 for c in range(1, len(lines)+1)} 
+cards = {c: 1 for c in range(1, len(lines) + 1)}
 total = 0
 while True:
-    next_cards = {c: 0 for c in range(1, len(lines)+1)}
-    for card in cards.keys():
+    next_cards = defaultdict(int)
+    for card in [c for c in cards.keys() if cards[c] >= 1]:
         total += cards[card]
-        for _ in range(cards[card]):
-            for i in range(1, matches_per_card[card]+1):
-                next_cards[card+i] += 1
-    
+        for i in range(1, matches_per_card[card] + 1):
+            next_cards[card + i] += cards[card]
+
     cards = next_cards
-    if sum(cards.values()) == 0:
+    if sum(cards.values()) == 0:  # none left
         break
 
 print(f"Part 2: {total}")
