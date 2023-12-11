3ac14a19f039102133581dd8accda31b635259fe
Sam Wheating <samwheating@gmail.com>
2023-12-11 11:12:51 -0800

    Added finished day 10 solution

--- 10/solution.py
+++ 10/solution.py
@@ -10,7 +10,7 @@ from typing import List
 # F is a 90-degree bend connecting south and east.
 
 # using this coord system
-#      
+#
 #       -y
 #        |
 #  -x ---+--- +x
@@ -49,86 +49,168 @@ TILES_TO_DIRECTIONS = {
 # O = outside
 # N = not visited
 
-def mark_edges(board: List[List[str]]) -> None:
+
+def mark_edges(board: List[List[str]]) -> List[List[str]]:
     # marks any non-pipe spots around the edge of the board as outside.
-    # returns None (all changes in-place)
-    for y in [0, len(board)-1]:
+    # this has side affects, as it modifies the original list.
+    for y in [0, len(board) - 1]:
         for x in range(len(board[0])):
-            if board[y][x] == "N":
-                board[y][x] = "O"
-    
-    for y in range(len(board)):
-        for x in [0, len(board[0])-1]:
-            if board[y][x] == "N":
+            if board[y][x] == ".":
                 board[y][x] = "O"
 
-def flood(board: List[List[str]], y: int, x: int) -> None:
-    edges = [(y,x)] # all of the nodes marked in the previous iteration
-    visited = [] # all of the nodes to mark at the end
-    inside = True # are these inside or outside
+    for y in range(len(board)):
+        for x in [0, len(board[0]) - 1]:
+            if board[y][x] == ".":
+                board[y][x] = "O"
+
+    return board
+
+
+def flood(board: List[List[str]], y: int, x: int) -> List[List[str]]:
+    edges = [(y, x)]  # all of the nodes marked in the previous iteration
+    visited = set()  # all of the nodes to mark at the end
+    inside = True  # are these inside or outside
     while True:
-        next_edges = []
+        next_edges = set()
         for node in edges:
-            for y,x in [ # four surrounding nodes
-                (node[0]-1,node[1]),
-                (node[0]+1,node[1]),
-                (node[0],node[1]-1),
-                (node[0],node[1]+1)
+            for y, x in [  # four surrounding nodes
+                (node[0] - 1, node[1]),
+                (node[0] + 1, node[1]),
+                (node[0], node[1] - 1),
+                (node[0], node[1] + 1),
             ]:
-                if y < 0 or y > len(board)-1 or x < 0 or x > len(board[0])-1:
+                if y < 0 or y > len(board) - 1 or x < 0 or x > len(board[0]) - 1:
                     continue
-                if board[y][x] == "N" and (y,x) not in visited:
-                    next_edges.append((y,x))
-                if board[y][x] == "O": # this block of nodes is connected to the outside
+                if board[y][x] == "." and (y, x) not in visited:
+                    next_edges.add((y, x))
+                if (
+                    board[y][x] == "O"
+                ):  # this block of nodes is connected to the outside
                     inside = False
 
-        visited.extend(edges)
+        visited.update(edges)
         edges = next_edges
         if len(edges) == 0:
             break
 
     # now mark all of the tiles as either inside or outside
     mark = "I" if inside else "O"
-    for y,x in visited:
-            board[y][x] = mark
+    for y, x in visited:
+        board[y][x] = mark
 
-def count_enclosed_squares(board: List[List[str]]) -> int:
+    return board
+
+
+def triple_board(board: List[List[str]]) -> List[List[str]]:
     """
-    1) mark all of the non-pipe edges as outside
-    2) flood fill every single non-visited node
-    3) return the total number of I squares 
+    scales up the board, turning something like
+
+    F-7
+    |.|
+    L-J
+
+    into :
+
+    .........
+    .FF---77.
+    .F.....7.
+    .|.....|.
+    .|.....|.
+    .|.....|.
+    .L.....J.
+    .LL---JJ.
+    .........
+
+    since this allows for flood filling between the gaps between ||, etc
+
+    Also preserves the original symbols for easier debugging / viz.
     """
-    mark_edges(board)
+
+    new_board = [["." for _ in range(len(board[0]) * 3)] for _ in range(len(board) * 3)]
     for y in range(len(board)):
         for x in range(len(board[y])):
-            if board[y][x] == "N":
-                flood(board, y, x)
+            if board[y][x] == "-":
+                new_board[y * 3 + 1][x * 3] = "-"
+                new_board[y * 3 + 1][x * 3 + 1] = "-"
+                new_board[y * 3 + 1][x * 3 + 2] = "-"
 
+            elif board[y][x] == "|":
+                new_board[y * 3][x * 3 + 1] = "|"
+                new_board[y * 3 + 1][x * 3 + 1] = "|"
+                new_board[y * 3 + 2][x * 3 + 1] = "|"
+
+            elif board[y][x] == "F":
+                new_board[y * 3 + 2][x * 3 + 1] = "F"
+                new_board[y * 3 + 1][x * 3 + 1] = "F"
+                new_board[y * 3 + 1][x * 3 + 2] = "F"
+
+            elif board[y][x] == "7":
+                new_board[y * 3 + 2][x * 3 + 1] = "7"
+                new_board[y * 3 + 1][x * 3 + 1] = "7"
+                new_board[y * 3 + 1][x * 3] = "7"
+
+            elif board[y][x] == "J":
+                new_board[y * 3][x * 3 + 1] = "J"
+                new_board[y * 3 + 1][x * 3 + 1] = "J"
+                new_board[y * 3 + 1][x * 3] = "J"
+
+            elif board[y][x] == "L":
+                new_board[y * 3][x * 3 + 1] = "L"
+                new_board[y * 3 + 1][x * 3 + 1] = "L"
+                new_board[y * 3 + 1][x * 3 + 2] = "L"
+
+    return new_board
+
+
+def count_3x3_squares(board: List[List[str]], char="I") -> int:
     total = 0
-    for y in range(len(board)):
-        for x in range(len(board[y])):
-            if board[y][x] == "I":
+    for y in range(len(board) // 3):
+        for x in range(len(board[y]) // 3):
+            empty = True
+            for dy in range(3 * y, 3 * y + 3):
+                for dx in range(3 * x, 3 * x + 3):
+                    if board[dy][dx] != char:
+                        empty = False
+            if empty:
                 total += 1
 
     return total
 
 
-if __name__ == "__main__":
+def count_enclosed_squares(board: List[List[str]]) -> int:
+    """
+    1) mark all of the non-pipe edges as outside
+    2) flood fill every single non-visited node
+    3) return the total number of I squares
+    """
 
+    board = triple_board(board)
+    board = mark_edges(board)
+    for y in range(len(board)):
+        for x in range(len(board[y])):
+            if board[y][x] == ".":
+                board = flood(board, y, x)
+
+    return count_3x3_squares(board, "I")
+
+
+if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "--test":
         file = "test_input.txt"
-        STARTING_TILE = "F" # just gonna hardcode this, not worth implementing it
-        STARTING_DIRECTION = (0,1) # >
+        STARTING_TILE = (
+            "F"  # just gonna hardcode this, not worth the time to implement it
+        )
+        STARTING_DIRECTION = (0, 1)  # >
     else:
         file = "input.txt"
         STARTING_TILE = "|"
-        STARTING_DIRECTION = (1,0) # ^
+        STARTING_DIRECTION = (1, 0)  # ^
 
     with open(file) as inputfile:
         lines = [l.strip("\n") for l in inputfile.readlines()]
 
     # build a maze from the input rows
-    init_x, init_y = 0,0
+    init_x, init_y = 0, 0
     maze = {y: {} for y in range(len(lines))}
     for y in range(len(lines)):
         for x in range(len(lines[y])):
@@ -140,17 +222,19 @@ if __name__ == "__main__":
                 maze[y][x] = lines[y][x]
 
     # follow the maze and count the distance
-    x,y = init_x, init_y
+    x, y = init_x, init_y
     dist = 0
     direction = STARTING_DIRECTION
-    board = [['N' for _ in range(len(lines[0]))] for _ in range(len(lines))]
+    board = [["." for _ in range(len(lines[0]))] for _ in range(len(lines))]
     while True:
         # step position then rotate according to tile + direction
         board[y][x] = maze[y][x]
-        y,x = y + direction[0], x + direction[1]
+        y, x = y + direction[0], x + direction[1]
         dist += 1
         direction = TILES_TO_DIRECTIONS[maze[y][x]][direction]
         if x == init_x and y == init_y:
             break
 
-    print(f"Part 1: {dist//2}") # Its a loop, so the furthest point is halfway
+    print(f"Part 1: {dist//2}")  # Its a loop, so the furthest point is halfway
+
+    print(f"Part 2{count_enclosed_squares(board)}")
--- 10/test_solution.py
+++ 10/test_solution.py
@@ -1,73 +1,109 @@
 # moving tests out to their own file...
 
-from solution import mark_edges, flood, count_enclosed_squares
+from solution import (
+    mark_edges,
+    flood,
+    count_enclosed_squares,
+    triple_board,
+    count_3x3_squares,
+)
 
 test_board = [
-                ["N", "F", "-", "7"],
-                ["N", "|", "N", "|"],
-                ["N", "L", "-", "J"],
-                ["N", "N", "N", "N"]
-            ]
+    [".", "F", "-", "7"],
+    [".", "|", ".", "|"],
+    [".", "L", "-", "J"],
+    [".", ".", ".", "."],
+]
 
-mark_edges(test_board)
-
-assert test_board == [
-                ["O", "F", "-", "7"],
-                ["O", "|", "N", "|"],
-                ["O", "L", "-", "J"],
-                ["O", "O", "O", "O"]
-            ]
+assert mark_edges(test_board) == [
+    ["O", "F", "-", "7"],
+    ["O", "|", ".", "|"],
+    ["O", "L", "-", "J"],
+    ["O", "O", "O", "O"],
+]
 
 # Testing the flood-fill implementation
 
 test_board = [
-                ["◼️", "◼️", "◼️", "◼️"],
-                ["◼️", "N", "N", "◼️"],
-                ["◼️", "◼️", "N", "◼️"],
-                ["N", "◼️", "◼️", "◼️"]
-            ]
+    ["◼️", "◼️", "◼️", "◼️"],
+    ["◼️", ".", ".", "◼️"],
+    ["◼️", "◼️", ".", "◼️"],
+    [".", "◼️", "◼️", "◼️"],
+]
+
+flood(test_board, 1, 1)
+
+assert flood(test_board, 1, 1) == [
+    ["◼️", "◼️", "◼️", "◼️"],
+    ["◼️", "I", "I", "◼️"],
+    ["◼️", "◼️", "I", "◼️"],
+    [".", "◼️", "◼️", "◼️"],
+]
+
+
+test_board = [
+    ["◼️", "◼️", "◼️", "◼️"],
+    ["◼️", ".", ".", "◼️"],
+    ["◼️", ".", ".", "◼️"],
+    ["◼️", "◼️", "O", "◼️"],
+]
 
 flood(test_board, 1, 1)
 
 assert test_board == [
-                ["◼️", "◼️", "◼️", "◼️"],
-                ["◼️", "I", "I", "◼️"],
-                ["◼️", "◼️", "I", "◼️"],
-                ["N", "◼️", "◼️", "◼️"]
-            ]
+    ["◼️", "◼️", "◼️", "◼️"],
+    ["◼️", "O", "O", "◼️"],
+    ["◼️", "O", "O", "◼️"],
+    ["◼️", "◼️", "O", "◼️"],
+]
 
+assert count_enclosed_squares([["F", "-", "7"], ["|", ".", "|"], ["L", "-", "J"]]) == 1
+
+# testing scaling up the board
+
+assert triple_board([["-"]]) == [[".", ".", "."], ["-", "-", "-"], [".", ".", "."]]
+
+assert triple_board([["|"]]) == [[".", "|", "."], [".", "|", "."], [".", "|", "."]]
+
+assert triple_board([["F"]]) == [[".", ".", "."], [".", "F", "F"], [".", "F", "."]]
+
+assert triple_board([["L"]]) == [[".", "L", "."], [".", "L", "L"], [".", ".", "."]]
+
+assert triple_board([["J"]]) == [[".", "J", "."], ["J", "J", "."], [".", ".", "."]]
+
+assert triple_board([["7"]]) == [[".", ".", "."], ["7", "7", "."], [".", "7", "."]]
+
+expected = [
+    list(row)
+    for row in """
+.........
+.FF---77.
+.F.....7.
+.|.....|.
+.|.....|.
+.|.....|.
+.L.....J.
+.LL---JJ.
+.........
+""".split(
+        "\n"
+    )
+    if len(row) != 0
+]
+
+assert triple_board([["F", "-", "7"], ["|", ".", "|"], ["L", "-", "J"]]) == expected
+
+assert count_3x3_squares([[".", ".", "."], [".", ".", "."], [".", ".", "."]], ".") == 1
+assert count_3x3_squares([[".", ".", "."], [".", ".", "."], [".", ".", "F"]], ".") == 0
+
+assert count_3x3_squares(triple_board([[".", "."], [".", "."]]), ".") == 4
+assert count_3x3_squares(triple_board([["-", "."], [".", "-"]]), ".") == 2
 
 test_board = [
-                ["◼️", "◼️", "◼️", "◼️"],
-                ["◼️", "N", "N", "◼️"],
-                ["◼️", "N", "N", "◼️"],
-                ["◼️", "◼️", "O", "◼️"]
-            ]
-
-flood(test_board, 1, 1)
-
-assert test_board == [
-                ["◼️", "◼️", "◼️", "◼️"],
-                ["◼️", "O", "O", "◼️"],
-                ["◼️", "O", "O", "◼️"],
-                ["◼️", "◼️", "O", "◼️"]
-            ]
-
-
-# Testing the full part 2 code
-
-test_board = [
-                ["◼️", "◼️", "◼️", "◼️"],
-                ["◼️", "N", "N", "◼️"],
-                ["◼️", "N", "N", "◼️"],
-                ["◼️", "◼️", "◼️", "◼️"]
-            ]
-
-assert count_enclosed_squares(test_board) == 4
-
-test_board = [list(row) for row in """
+    list(row)
+    for row in """
 ...........
-.S-------7.
+.F-------7.
 .|F-----7|.
 .||.....||.
 .||.....||.
@@ -75,13 +111,21 @@ test_board = [list(row) for row in """
 .|..|.|..|.
 .L--J.L--J.
 ...........
-""".replace(".", "N").split("\n") if len(row) != 0]
+""".replace(
+        ".", "."
+    ).split(
+        "\n"
+    )
+    if len(row) != 0
+]
 
 assert count_enclosed_squares(test_board) == 4
 
-test_board = [list(row) for row in """
+test_board = [
+    list(row)
+    for row in """
 ..........
-.S------7.
+.F------7.
 .|F----7|.
 .||....||.
 .||....||.
@@ -89,21 +133,35 @@ test_board = [list(row) for row in """
 .|..||..|.
 .L--JL--J.
 ..........
-""".replace(".", "N").split("\n") if len(row) != 0]
+""".replace(
+        ".", "."
+    ).split(
+        "\n"
+    )
+    if len(row) != 0
+]
 
 assert count_enclosed_squares(test_board) == 4
 
-test_board = [list(row) for row in """
+test_board = [
+    list(row)
+    for row in """
 .F----7F7F7F7F-7....
 .|F--7||||||||FJ....
 .||.FJ||||||||L7....
 FJL7L7LJLJ||LJ.L-7..
-L--J.L7...LJS7F-7L7.
+L--J.L7...LJF7F-7L7.
 ....F-J..F7FJ|L7L7L7
 ....L7.F7||L7|.L7L7|
 .....|FJLJ|FJ|F7|.LJ
 ....FJL-7.||.||||...
 ....L---J.LJ.LJLJ...
-""".replace(".", "N").split("\n") if len(row) != 0]
+""".replace(
+        ".", "."
+    ).split(
+        "\n"
+    )
+    if len(row) != 0
+]
 
 assert count_enclosed_squares(test_board) == 8
